ac0dbd76eace45acb641ce4f678c0351a9a04c55
Caleb Knight <knightca@oregonstate.edu>
2021-11-02 11:50:01 -0700

    Created c06 and updated parser.h

---
+++ cploration/c06/parser.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#define MAX_LINE_LENGTH 200
+#define MAX_LABEL_LENGTH MAX_LINE_LENGTH-2
+
+typedef int16_t hack_addr;
+typedef int16_t opcode;
+
+char *strip(char *s);
+void parse(FILE * file);
+bool is_Atype(const char *);
+bool is_label(const char *);
+bool is_Ctype(const char *);
