a1c4e29616f6c3c21f390cca1780e8c610b01022
Caleb Knight <knightca@oregonstate.edu>
2021-11-04 08:57:19 -0700

    Implemented mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,36 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+
+// Caleb Knight
+
+//R2=0
+  @R2
+  M=0
+
+//Load R0 and set value to Data
+  @R0
+  D=M
+  @INFINITE_LOOP
+  D;JEQ //If R0 == 0 Jump out
+
+//Load R1 and set value to Data
+  @R1
+  D=M
+  @INFINITE_LOOP
+  D;JEQ //If R1 == 0 Jump out
+
+//Loop through adding R1 Data to R2
+(LOOP)
+  @R1
+  D=M
+  @R2
+  M=D+M
+  @R0
+  D=M-1
+  M=D
+  @LOOP
+  D;JGT
+(INFINITE_LOOP)
+  @INFINITE_LOOP
+  0;JMP
