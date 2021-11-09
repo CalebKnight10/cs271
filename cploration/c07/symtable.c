ebfc58fc2e39a757f8ffdd60c3809b2232e19c81
Caleb Knight <knightca@oregonstate.edu>
2021-11-09 13:34:05 -0800

    Implemented find and display_table

--- cploration/c07/symtable.c
+++ cploration/c07/symtable.c
@@ -2,9 +2,7 @@
 
 // Unsigned long extended size variable for storing numbers (32 bits or 4 bytes)
 // Won't store negatives... range [0-2^32-1]
-unsigned long
-hash(unsigned char *str)
-{
+unsigned long hash(unsigned char *str) {
 	unsigned long hash = 5381;
 	int c;
 
@@ -19,21 +17,55 @@ hash(unsigned char *str)
 
 void insert(hack_addr addr,char *name[]) {
 
-   struct Symbol *item = (struct Symbol*) malloc(sizeof(struct Symbol));
-   item->name = name;  
-   item->addr = addr;
+	struct Symbol *item = (struct Symbol*) malloc(sizeof(struct Symbol));
+	item->name = name;  
+	item->addr = addr;
 
    //get the hash 
-   int hashIndex = hashCode(addr);
+	int hashIndex = hashCode(addr);
 
    //move in array until an empty or deleted cell
-   while(hashArray[hashIndex] != NULL && hashArray[hashIndex]->addr != NULL) {
+	while(hashArray[hashIndex] != NULL && hashArray[hashIndex]->addr != NULL) {
       //go to next cell
-      ++hashIndex;
+		++hashIndex;
 		
       //wrap around the table
-      hashIndex %= SIZE;
-   }
+		hashIndex %= SIZE;
+	}
 	
-   hashArray[hashIndex] = item;
+	hashArray[hashIndex] = item;
+}
+
+struct Symbol *find(char *name[]) {
+   //get the hash 
+	int hashIndex = hashCode(name);  
+	
+   //move in array until an empty 
+	while(hashArray[hashIndex] != NULL) {
+
+		if(hashArray[hashIndex]->name == name)
+			return hashArray[hashIndex]; 
+
+      //go to next cell
+		++hashIndex;
+		
+      //wrap around the table
+		hashIndex %= SIZE;
+	}        
+	
+	return NULL;        
+}
+
+void display_table() {
+	int i = 0;
+	
+	for(i = 0; i<SIZE; i++) {
+
+		if(hashArray[i] != NULL)
+			printf(" (%d,%d)",hashArray[i]->name,hashArray[i]->addr);
+		else
+			printf(" ~~ ");
+	}
+	
+	printf("\n");
 }
