8f096527edea5e884a2ce37e75d31d23ef1a9a7a
Caleb Knight <knightca@oregonstate.edu>
2021-11-23 09:24:50 -0800

    Created files and updated hack.h to reflect exercise 3

---
+++ cploration/c09/error.c
@@ -0,0 +1,22 @@
+#include "error.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+const char *error_messages[] = {
+	[EXIT_INCORRECT_ARGUMENTS] = "Usage: %s [filename]", 
+	[EXIT_CANNOT_OPEN_FILE] = "Cannot open file %s", 
+	[EXIT_TOO_MANY_INSTRUCTIONS] = "File contains than the maximum %u instructions", 
+	[EXIT_INVALID_LABEL] = "Line %u: %s: Invalid label name", 
+	[EXIT_SYMBOL_ALREADY_EXISTS] = "ine %u: %s: Symbol is already defined", 
+}; 
+
+void exit_program(enum exitcode code, ...) {
+	va_list arguments;
+	va_start(arguments, code);
+
+	printf("ERROR: ");
+	vfprintf(stdout, error_messages[code], arguments);
+
+	va_end(arguments);
+	exit(code);
+}
---
+++ cploration/c09/parser.h
@@ -0,0 +1,58 @@
+#ifndef __PARSER_H__
+#define __PARSER_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#define MAX_LINE_LENGTH 200
+#define MAX_LABEL_LENGTH MAX_LINE_LENGTH-2
+#define MAX_HASH_ADDRESS INT16_MAX
+#define MAX_INSTRUCTION MAX_HASH_ADDRESS
+
+typedef int16_t hack_addr;
+typedef int16_t opcode;
+
+typedef struct C_instruction {
+	opcode a:1;
+	opcode comp:6;
+	opcode dest:3;
+	opcode jump:3;
+} c_instruction;
+
+typedef struct A_instruction {
+	union AddyOrLabel {
+		hack_addr addy;
+		char *label;
+	} AddyOrLabel;
+	bool is_addr;
+} a_instruction;
+
+typedef enum instr_type {
+	Inv_instr = -1, 
+	A_instr, 
+	C_instr
+} instr_type;
+
+typedef struct Instruction {
+	union AorC {
+		a_instruction a;
+		c_instruction c;
+	} AorC;
+	 instr_type type_stored;
+} instruction;
+
+char *strip(char *s);
+char *extract_label(const char *line, char* label);
+
+void parse(FILE * file);
+
+bool is_Atype(const char *);
+bool is_label(const char *);
+bool is_Ctype(const char *);
+
+
+#endif
