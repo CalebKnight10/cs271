4f21c2e4fb6331c9d30ef53534b7122b49b01efb
Caleb Knight <knightca@oregonstate.edu>
2021-11-30 08:13:35 -0800

    Added test file

--- cploration/c09/error.h
+++ cploration/c09/error.h
@@ -8,7 +8,7 @@ typedef enum exitcode {
  * Error code 1 represents that given file does not exist.
  */
 	EXIT_INCORRECT_ARGUMENTS = 1,
-	EXIT_CANNOT_OPEN_FILE = 2,
+	EXIT_CANEXIT_CANNOT_OPEN_FILENOT_OPEN_FILE = 2,
 	EXIT_TOO_MANY_INSTRUCTIONS = 3, 
 	EXIT_INVALID_LABEL = 4, 
 	EXIT_SYMBOL_ALREADY_EXISTS = 5, 
