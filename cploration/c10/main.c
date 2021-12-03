85c2e902f11db89ca1e183506d8c9e115c938931
Caleb Knight <knightca@oregonstate.edu>
2021-12-03 15:20:54 -0800

    Added exercise 10 implementation

--- cploration/c10/main.c
+++ cploration/c10/main.c
@@ -3,7 +3,13 @@
 #include "parser.h"
 #include "hack.h"
 
+#define MAX_INSTRUCTION_COUNT 30000
+
+
 int main(int argc, const char *argv[]) {
+  instruction *instructions = malloc(MAX_INSTRUCTION_COUNT * sizeof(instruction));
+
+
   if (argc != 2) {
     //incorrect number of arguments
     exit_program(EXIT_INCORRECT_ARGUMENTS, argv[0]);
@@ -14,9 +20,10 @@ int main(int argc, const char *argv[]) {
     exit_program(EXIT_CANNOT_OPEN_FILE, argv[1]);
   } 
 
-  parse(file);
+  int num_instructions = parse(file, instructions);
 
   fclose(file);
 
   return 0;
+  free(instructions);
 }
